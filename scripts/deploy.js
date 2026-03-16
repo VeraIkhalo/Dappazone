a063408956af77804de9ca28ad74a9c66011fd1c
veraikhalo <ikhalo.oseaghe@gmail.com>
2026-03-16 16:46:48 +0100

    Add Dappazon contract with item listing and purchasing functionality; update frontend to display items and handle user interactions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,11 +7,37 @@
 const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
+const { ethers } = require("hardhat");
+
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  // Deploy Dappazon
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+
+  // Listing items...
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 
 }
 
